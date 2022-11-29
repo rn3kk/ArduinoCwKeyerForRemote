298df7679fff78f75407c120bd1b5e78f99aec06
nbanshchikov <rn3kk.nick@gmail.com>
2022-11-29 17:06:51 +0300

    вывод в ком порт идет при окончании символа

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -5,7 +5,7 @@ const int EE_ADR_WPM = 1;
 
 const int out_cw = 8;
 const int dashPin = 5; 
-const int ditPin = 3; 
+const int dotPin = 3; 
 const int ledPin = 13; 
 
 int wpm = 24;
@@ -13,31 +13,35 @@ int wpm = 24;
 int dot_len;
 int dash_len = dot_len*3;
 
+int pause_beetween_char = dot_len * 3;
+int pause_beetween_word = dot_len * 6;
+
 const char DASH = '-';
 const char DOT = '.';
 const char CHANR_SEPARATOR = ']';
 const char WORD_SEPARATOR = ' ';
 
-unsigned long last_dash_or_dot_time_send = millis();
-int count_empty_separator_sended = 0;
+String one_char = "";
+bool end_of_char_sended = false;
+unsigned long last_dash_or_dot_time = millis();
 
 void setup() {
   pinMode(ledPin, OUTPUT);  
   pinMode(out_cw, OUTPUT);  
 
   pinMode(dashPin, INPUT_PULLUP);   
-  pinMode(ditPin, INPUT_PULLUP);
+  pinMode(dotPin, INPUT_PULLUP);
 
   wpm = EEPROM.read(EE_ADR_WPM);
-  dot_len = 1.2 / wpm*1000;
-  dash_len = dot_len*3;
+  calculate_timings(wpm);
+
   Serial.begin(9600);
   send_params_to_serial("wpm", wpm);
   send_params_to_serial("dot", dot_len);
   send_params_to_serial("dash", dash_len);
 }
 
-void loop(){
+void loop(){  
   if (Serial.available() > 0)
   {
     String str = Serial.readString();       
@@ -54,46 +58,56 @@ void loop(){
       {
         wpm = 10;
       }
-      dot_len = 1.2 / float(wpm)*1000;
-      dash_len = dot_len*3;
+      calculate_timings(wpm);
       EEPROM.write(EE_ADR_WPM, wpm);
       send_params_to_serial("wpm", wpm);
       send_params_to_serial("dot", dot_len);
       send_params_to_serial("dash", dash_len);
     }
   }
-  else if (digitalRead(dashPin) == LOW) //Тире
-  {
-    Serial.print(DASH);
-    Serial.flush();
-    last_dash_or_dot_time_send = millis();
-    count_empty_separator_sended = 0;
 
+  if (digitalRead(dashPin) == LOW) //Тире
+  {
+    send_word_separator();
+    one_char = one_char + "-";    
+    end_of_char_sended = false;
     digitalWrite(out_cw, HIGH);
     delay(3*dot_len);
     digitalWrite(out_cw, LOW);
+    last_dash_or_dot_time = millis();
     delay(dot_len);
-
   }
-  else if (digitalRead(ditPin) == LOW) //Тире
-  {
-    Serial.print(DOT);
-    Serial.flush();
-    last_dash_or_dot_time_send = millis();
-    count_empty_separator_sended = 0;
 
+  if (digitalRead(dotPin) == LOW) //Тире
+  {
+    send_word_separator();
+    one_char = one_char + ".";    
+    end_of_char_sended = false;
     digitalWrite(out_cw, HIGH);
     delay(dot_len);
     digitalWrite(out_cw, LOW);
-    delay(dot_len);
+    last_dash_or_dot_time = millis();
+    delay(dot_len);    
   }
-  else
-  {    
-    if(millis() - last_dash_or_dot_time_send > dash_len && count_empty_separator_sended < 3)
+  if((millis() - last_dash_or_dot_time) > dot_len*3 && !end_of_char_sended)
+  {
+    Serial.println(one_char+"]");
+    Serial.flush();
+    one_char = "";    
+    end_of_char_sended = true;
+  }
+}
+
+void send_word_separator()
+{
+  if (end_of_char_sended)
+  {
+    unsigned long paus = millis() - last_dash_or_dot_time;
+    if ( paus >= pause_beetween_word )
     {
-      Serial.print(CHANR_SEPARATOR);
-      last_dash_or_dot_time_send = millis();
-      count_empty_separator_sended ++;      
+      one_char = one_char + " ";
+      //Serial.println(' ');
+      //Serial.flush();      
     }
   }
 }
@@ -104,4 +118,12 @@ void send_params_to_serial(String cmd, int value)
   Serial.print(value);
   Serial.print("\n");
   Serial.flush();
+}
+
+void calculate_timings(int wpm)
+{
+  dot_len = 1.2 / wpm*1000;
+  dash_len = dot_len*3;
+  pause_beetween_char = dot_len * 3;
+  pause_beetween_word = dot_len * 6 ;
 }
